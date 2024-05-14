81c186426312cc93e55e3a3ab3476b80efcd0a0d
Guzman <francisco.guzman@accenture.com>
2024-05-14 16:13:53 -0400

    Prime Time

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-//make a loop that goes from 1 to 100
-function FizzBuzz() {
-  for (let num = 1; num < 16; num++) {
+// //make a loop that goes from 1 to 100
+// function FizzBuzz() {
+//   for (let num = 1; num < 16; num++) {
     
-    if (num % 5 == 0 && num % 3 == 0) {
+//     if (num % 5 == 0 && num % 3 == 0) {
      
-        console.log("FizzBuzz");
+//         console.log("FizzBuzz");
+//     }
+//     // number divisible by 3 log "Fizz"
+//     if (num % 3 == 0) {
+//       console.log("Fizz");
+//     } else if (num % 5 == 0) {
+//       console.log("Buzz");
+//     } else console.log(num);
+//   }
+// }
+
+// FizzBuzz();
+
+function checkPrime(num) {
+  let i = 2;
+ 
+  while (i < num) {
+    if (num % i === 0) {
+      return false;
     }
-    // number divisible by 3 log "Fizz"
-    if (num % 3 == 0) {
-      console.log("Fizz");
-    } else if (num % 5 == 0) {
-      console.log("Buzz");
-    } else console.log(num);
+    i++;
   }
+  return [true, i];
 }
-FizzBuzz();
+ 
+function nextPrimeNumber(startingNum) {
+  let starting = startingNum + 1;
+ 
+  while (starting) {
+    if (checkPrime(starting)) {
+      break;
+    }
+ 
+    starting++;
+  }
+  return starting;
+}
+ 
+console.log(nextPrimeNumber(20));
